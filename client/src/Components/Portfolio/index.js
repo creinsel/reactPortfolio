f1f9e230558efce8b01006aa6cf15801f118fc71
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-08-30 12:30:56 -0700

    adding card to port comp

--- client/src/Components/Portfolio/index.js
+++ client/src/Components/Portfolio/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
-const Portfolio = () => {
+const Portfolio = (props) => {
     return (
-<>
+<div>
 <Jumbotron>
   <h1>Portfolio</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-   LUKERFTGHK
-  </p>
+  <div className="card">
+  <div className="card-body">
+    <p className="card-text">This image is in the middle</p>
+  </div>
+  <img src={props.image} alt={props.name}/>
+  <div className="card-body">
+    <p className="card-text">of a card.</p>
+  </div>
+  </div>
 </Jumbotron>  
-</>
+</div>
     )
 }
 
