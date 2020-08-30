9423bd9f369f58fef298c347c5fdd6acb154ca00
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-08-30 13:09:36 -0700

    map projects

--- client/src/Components/Portfolio/index.js
+++ client/src/Components/Portfolio/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import ProjCard from '../ProjCard';
+import projects from '../../projects.json';
 
 
 const Portfolio = (props) => {
     return (
 <div>
 <Jumbotron>
-  <ProjCard/>
+  {this.projects.map(project => (
+    <ProjCard
+    key={project.id}
+    name={project.name}
+    image={project.image}
+    description={project.description}
+    technologies={project.technologies}
+    deployed={project.deployed}
+    repo={project.repo}
+    />
+  ))}
+  
 </Jumbotron>  
 </div>
     )
