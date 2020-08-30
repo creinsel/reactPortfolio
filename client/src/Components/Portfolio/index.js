7bd78f794a6a05fd6ce4b94540cf0d4dfe5a281b
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-08-30 12:41:19 -0700

    card component

--- client/src/Components/Portfolio/index.js
+++ client/src/Components/Portfolio/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import ProjCard from '../ProjCard';
 
 
 const Portfolio = (props) => {
     return (
 <div>
 <Jumbotron>
-  <h1>Portfolio</h1>
-  <div className="card">
-  <div className="card-body">
-    <p className="card-text">This image is in the middle</p>
-  </div>
-  <img src={props.image} alt={props.name}/>
-  <div className="card-body">
-    <p className="card-text">of a card.</p>
-  </div>
-  </div>
+  <ProjCard/>
 </Jumbotron>  
 </div>
     )
---
+++ client/src/Components/ProjCard/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+
+const ProjCard = (props) => {
+    return (
+<div>
+  <h1>Projects</h1>
+<Card>
+  <Card.Header>
+    <Nav variant="tabs" defaultActiveKey="#first">
+      <Nav.Item>
+        <Nav.Link href="#first">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  </Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>  
+</div>
+    )
+}
+
+export default ProjCard;
