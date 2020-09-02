1f666a9793cd5e378663759d3404cee259c9f1fa
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-09-02 10:08:33 -0700

    changed size of photo

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -9,14 +9,13 @@ const About = () => {
         <>
        <Jumbotron className="container top">
   <h1>About Section</h1>
-<span>
-  <img src= "assets/images/casImg.jpg"/>
-  
+
+  <img class="imgAbt" src= "assets/images/casImg.jpg"/>
   <p>
   Full Stack Developer with extensive experience in the education/public service industry.  Recently earned a certificate in Full Stack Development through the UC Berkeley Extension program. Familiar with technologies such as Javascript, HTML, Node.js, React, Express, Handlebars, etc. With a background as a former educator, extremely efficient at meeting deadlines, thinking critically, and using creativity to complete work.  Love learning from others in a collaborative environment and am passionate about creating web applications that are both functional and aesthetic. 
 .
   </p>
-  </span>
+  
 </Jumbotron>  
        
      </>
