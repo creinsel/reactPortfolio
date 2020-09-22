a04977f1bbc8b9d8010f060cb0cd032c58421124
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-09-22 12:10:48 -0700

    style card

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -7,7 +7,7 @@ const Nav = () => {
         
       <nav className="navbar navbar-expand-lg navbar-light bg-light m-1">
       <div className="logo">
-          {/* Link for app logo? */}
+          
 
           <h3>Cassondra <br></br> Reinsel</h3>
         </div>
@@ -16,13 +16,13 @@ const Nav = () => {
       <div className="collapse navbar-collapse nav-menu mr-auto justify-content-end">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/">About <span className="sr-only">(current)</span></a>
+            <a className="nav-link">About <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/projects">Projects</a>
+            <a className="nav-link" >Projects</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="/contact">Contact</a>
+            <a className="nav-link" >Contact</a>
           </li>
         </ul>
         
