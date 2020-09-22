a91aa4873e953c7aea780a83fb7e367d9f3ae535
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-09-22 11:35:25 -0700

    added style file

--- client/src/Components/Contact/index.js
+++ client/src/Components/Contact/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import './style.css';
 
 
 const Contact = () => {
@@ -8,7 +9,9 @@ const Contact = () => {
 <Jumbotron className="container">
   <h1>Contact</h1>
  
-  
+  <img src = "assets/images/email.png" alt= "email" class="icons"/>
+  <img src = "assets/images/giticon.png" alt= "git" class="icons"/>
+  <img src = "assets/images/linkedin.png" alt= "linkedin" class="icons"/>
 </Jumbotron>  
 </>
     )
