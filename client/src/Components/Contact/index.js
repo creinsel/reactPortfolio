85f545c6911ec317ca2a3ed15b44cab4e6c6d593
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-09-22 17:27:43 -0700

    style contacts

--- client/src/Components/Contact/index.js
+++ client/src/Components/Contact/index.js
@@ -9,10 +9,10 @@ const Contact = () => {
 <Jumbotron className="container">
   <h1>Contact</h1>
  
- <div className="justify-content-center">
-  <img src = "assets/images/emailN.jpg" alt= "email" class="icons"/>
-  <a href="https://github.com/creinsel" target="_blank"><img src = "assets/images/git.png" alt= "git" class="icons"/></a>
-  <a href= "https://www.linkedin.com/in/cassondra-reinsel-1a195840/"><img src = "assets/images/linked2.png" alt= "linkedin" class="icons"/></a>
+ <div className="contacts">
+  <img src = "assets/images/emailN.jpg" alt= "email" className="icons"/>
+  <a href="https://github.com/creinsel" target="_blank"><img src = "assets/images/git.png" alt= "git" className="icons"/></a>
+  <a href= "https://www.linkedin.com/in/cassondra-reinsel-1a195840/"><img src = "assets/images/linked2.png" alt= "linkedin" className="icons"/></a>
   </div>
 </Jumbotron>  
 </>
